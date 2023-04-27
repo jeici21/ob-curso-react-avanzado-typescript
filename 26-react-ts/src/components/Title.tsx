7b9ac122fdb072fc60ceb92000eacb565f98585c
Jorge Castro <david92_jc@yahoo.es>
2023-04-27 18:26:39 -0500

    correcciones menores

--- 26-react-ts/src/components/Title.tsx
+++ 26-react-ts/src/components/Title.tsx
@@ -1,7 +1,5 @@
 const Title = ({ title }: { title: string }) => {
-    return (
-        <h1 className="text-xl text-slate-700 text-left">{title}</h1>
-    )
+    return <h1 className="text-xl text-slate-700 text-left">{title}</h1>
 }
 
 export default Title
